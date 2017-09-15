7f188a4ebe690bb2a4200f9871b83fa84dfbf67f
Richelly Italo <richellyitalo@gmail.com>
2017-09-15 17:31:59 -0300

    Criação do primeiro e componente.

---
+++ my-app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <my-menu title="Meu Primeiro Componente">
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </my-menu>
+
+    <img src="http://vuejs.org/images/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import MyMenu from './MyMenu.vue';
+
+export default {
+  name: 'app',
+  components: {
+    MyMenu
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-app/src/MyMenu.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{title}}</h1>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Menu sem título'
+    }
+  }
+}
+</script>
